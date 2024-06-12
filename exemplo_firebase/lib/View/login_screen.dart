bbb7cf03833213f87111499465efb3170abf5571
Diogotb <digogo@gmail.com>
2024-06-12 16:04:21 -0300

    12/06

--- exemplo_firebase/lib/View/login_screen.dart
+++ exemplo_firebase/lib/View/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'register_screen.dart';
+import 'task_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
--- exemplo_firebase/lib/View/register_screen.dart
+++ exemplo_firebase/lib/View/register_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'task_screen.dart';
+
 class RegisterScreen extends StatefulWidget {
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
